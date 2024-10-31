15e78f9f4ba9a14c4f81ba005222b6a8762d8260
İlkay Ceren Kaleoğlu <ilkaycerenkaleoglu@gmail.com>
2024-11-01 01:06:21 +0300

    Add files via upload

---
+++ C#EgitimKampi301/C#EgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_EgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CadegoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+    }
+}
